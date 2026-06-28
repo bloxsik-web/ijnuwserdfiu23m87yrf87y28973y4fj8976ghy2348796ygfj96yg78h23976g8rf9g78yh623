c31fd5f59b88f321b5449e95e9d49fafa4e75b09
bloxsik-web <bloxsik.prod@gmail.com>
2026-06-29 01:25:04 +0700

    Create 2.lua

---
+++ 𝄃𝄃𝄂𝄂𝄀𝄁𝄃𝄂𝄂𝄃/2.lua
@@ -0,0 +1,5 @@
+local d=false; 
+local p=game.Players.LocalPlayer;
+local h=p.Character or p.CharacterAdded:Wait();  
+local m=h:FindFirstChild("Humanoid"); 
+local f=h:FindFirstChild("HumanoidRootPart");
